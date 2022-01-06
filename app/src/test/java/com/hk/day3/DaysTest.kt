59bee096e5ef473ca24c33d8a884750dcd2f598b
David <18588744489@163.com>
2022-01-06 17:39:03 +0800

    Update

---
+++ app/src/test/java/com/hk/day3/DaysTest.kt
@@ -0,0 +1,12 @@
+package com.hk.day3
+
+/**
+ * @name Hello-Kotlin
+ * @class name：com.hk.day3
+ * @class describe
+ * @anthor David
+ * @time 2022/1/6 5:20 下午
+ * @class describe
+ */
+class DaysTest {
+}
---
+++ app/src/test/java/com/hk/day3/KUser.kt
@@ -0,0 +1,11 @@
+package com.hk.day3
+
+/**
+ * @name Hello-Kotlin
+ * @class name：com.hk.day3
+ * @class describe
+ * @anthor David
+ * @time 2022/1/6 5:19 下午
+ * @class describe
+ */
+data class KUser()
