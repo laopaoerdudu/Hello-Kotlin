7f5964e916a71f936eca67a8d94cca01919e942f
David <18588744489@163.com>
2022-01-07 17:43:28 +0800

    Update

--- app/src/test/java/com/hk/day5/FunctionExt.kt
+++ app/src/test/java/com/hk/day5/FunctionExt.kt
@@ -5,4 +5,16 @@ val lambda: () -> Unit = {
     println("lambda 括号里面的函数最后一行就是这个函数的返回值")
 }
 
+val hhh: (Int) -> String = {
+    it.toString()
+}
+
 val f5 = { p: Int -> p + 10 }
+
+fun t1(params: (Int) -> String): String {
+    return params(10)
+}
+
+fun testT1() {
+    val name = ::t1
+}
