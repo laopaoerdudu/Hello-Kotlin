7e7128eb462e84d3f97ae3b26ffd9ffceb8c061a
David <18588744489@163.com>
2022-01-08 17:33:12 +0800

    Update

---
+++ app/src/test/java/com/hk/KotlinTest.kt
@@ -0,0 +1,135 @@
+package com.hk
+
+import org.junit.Test
+
+class KotlinTest {
+
+    @Test
+    fun test1() {
+        // hello()
+        hello().invoke()
+        hello()()
+    }
+
+    @Test
+    fun test2() {
+        // 原始字符串 -> 不支持转义字符
+        val world = "multiline world"
+        println(
+            """
+           Hello
+           \$world
+        """.trimIndent()
+        )
+    }
+
+    @Test
+    fun test3() {
+        printNumberSign(-2)
+        print(", ")
+        printNumberSign(0)
+        print(", ")
+        printNumberSign(2)
+    }
+
+    @Test
+    fun test4() {
+        // printNumberSign2 和 printNumberSign 等价，但是写法上看起来更清晰
+        printNumberSign2(-2)
+        print(", ")
+        printNumberSign2(0)
+        print(", ")
+        printNumberSign2(2)
+    }
+
+    @Test
+    fun test5() {
+        printNumberSign3(-2)
+        print(", ")
+        printNumberSign3(0)
+        print(", ")
+        printNumberSign3(2)
+    }
+
+    @Test
+    fun test6() {
+        run()
+    }
+
+    @Test
+    fun test7() {
+        // 函数类型变量的两种调用方式：(variable)() 和 variable.invoke()
+        run1()
+    }
+
+    private fun run1() {
+//        val run: () -> Unit = {
+//            println("Run run run!")
+//        }
+//
+//        fun run() = println("Hello World")
+//
+//        Runnable { run.invoke() }.run()
+
+        // 或者这样也可以
+        val run: () -> Unit = {
+            println("Run run run!")
+        }
+
+        fun run() = println("Hello World")
+
+        Runnable { (run)() }.run()
+    }
+
+    private fun run() {
+        val run: () -> Unit = {
+            println("Run run run!")
+        }
+
+        // 新增一个 run 函数
+        fun run() = println("Hello World")
+
+        Runnable { run() }.run()
+    }
+
+
+    private fun printNumberSign(num: Int) {
+        // Kotlin 中没有类似于 Java 的三目运算符，取而代之的是 if-else 表达式，
+        // let 函数作用于它前面的对象
+        if (num < 0) {
+            "negative"
+        } else if (num > 0) {
+            "positive"
+        } else {
+            "zero"
+        }.let { print(it) }
+    }
+
+    private fun printNumberSign2(num: Int) {
+        if (num < 0) {
+            "negative"
+        } else {
+            if (num > 0) {
+                "positive"
+            } else {
+                "zero"
+            }.let { print(it) }
+        }
+    }
+
+    private fun printNumberSign3(num: Int) {
+        (if (num < 0) {
+            "negative"
+        } else {
+            if (num > 0) {
+                "positive"
+            } else {
+                "zero"
+            }
+        }).let { print(it) }
+    }
+
+    private fun hello(): () -> Unit = {
+        println("Hello, World")
+    }
+}
--- app/src/test/java/com/hk/day5/FunctionExt.kt
+++ app/src/test/java/com/hk/day5/FunctionExt.kt
@@ -18,3 +18,20 @@ fun t1(params: (Int) -> String): String {
 fun testT1() {
     val name = ::t1
 }
+
+val increment = { i: Int -> i + 1 }
+val bicrement = { i: Int -> i + 2 }
+val double = { i: Int -> i * 2 }
+val one = { 1 }
+
+infix fun <T, R> (() -> T).then(another: (T) -> R): () -> R = { another(this()) }
+operator fun <T, R1, R2> ((T) -> R1).plus(another: (T) -> R2) = { x: T -> this(x) to another(x) }
+
+fun main(args: Array<String>) {
+    val equilibrum = one then double then (increment + bicrement)
+    print(equilibrum())
+}
+
+// (one then double) -> 2
+// (increment + bicrement) -> (i+1) to (i+2)
+// 2 then ((i+1), (i+2))
